8beb98f5270b0a1c5f7fa61b8886bf6629bc2560
tstephenson724 <tstephenson724@41880c4b-a73f-416b-817f-a161b90437aa>
2014-02-17 23:53:45 +0000

    Prototype of activiti-spring-rest
    git-svn-id: https://svn-bigbpmcloud.forge.cloudbees.com/platform@55 41880c4b-a73f-416b-817f-a161b90437aa

---
+++ activiti-spring-rest/trunk/src/test/java/org/activiti/spring/rest/model/FormDataOnDemand.java
@@ -0,0 +1,7 @@
+package org.activiti.spring.rest.model;
+import org.activiti.spring.rest.model.Form;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Form.class)
+public class FormDataOnDemand {
+}
---
+++ activiti-spring-rest/trunk/src/test/java/org/activiti/spring/rest/model/FormPropertyDataOnDemand.java
@@ -0,0 +1,7 @@
+package org.activiti.spring.rest.model;
+import org.activiti.spring.rest.model.FormProperty;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = FormProperty.class)
+public class FormPropertyDataOnDemand {
+}
