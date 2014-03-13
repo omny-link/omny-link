c3a31ca672c4600e2675853abb7c133244c4b597
tstephenson724 <tstephenson724@41880c4b-a73f-416b-817f-a161b90437aa>
2014-03-13 21:23:33 +0000

    - Added signUpDate automatically - Added unordered findByExample - Restored transactionManager to the activiti rest library - Some more logging in the deployment controllers
    git-svn-id: https://svn-bigbpmcloud.forge.cloudbees.com/platform@70 41880c4b-a73f-416b-817f-a161b90437aa

--- activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/web/ReportableException.java
+++ activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/web/ReportableException.java
@@ -14,40 +14,41 @@ import org.slf4j.LoggerFactory;
  */
 public class ReportableException extends Exception {
 
-	private static final long serialVersionUID = 786708592317L;
+    private static final long serialVersionUID = 786708592317L;
 
-	protected static final Logger LOGGER = LoggerFactory
-			.getLogger(ReportableException.class);
+    protected static final Logger LOGGER = LoggerFactory
+            .getLogger(ReportableException.class);
 
-	private String details;
+    private String details;
 
-	public ReportableException(String msg) {
-		super(msg);
-		LOGGER.error(toLog());
-	}
+    public ReportableException(String msg) {
+        super(msg);
+        LOGGER.error(toLog());
+    }
 
-	public ReportableException(String msg, String details) {
-		super(msg);
-		LOGGER.error(toLog());
-		this.details = details;
-	}
+    public ReportableException(String msg, String details) {
+        super(msg);
+        LOGGER.error(toLog());
+        this.details = details;
+    }
 
-	public ReportableException(String msg, Exception cause) {
-		super(msg, cause);
-		if (cause.getStackTrace().length> 0){ 
-			this.details = cause.getStackTrace()[0].getClassName() + "."
-					+ cause.getStackTrace()[0].getMethodName() + "("
-					+ cause.getStackTrace()[0].getLineNumber() + ")";
-		}
-	}
+    public ReportableException(String msg, Exception cause) {
+        super(msg, cause);
+        LOGGER.error(toLog(), cause);
+        if (cause.getStackTrace().length > 0) {
+            this.details = cause.getStackTrace()[0].getClassName() + "."
+                    + cause.getStackTrace()[0].getMethodName() + "("
+                    + cause.getStackTrace()[0].getLineNumber() + ")";
+        }
+    }
 
-	public String toJson() {
-		return String.format("{\"error\":\"%1$s\",\"details\":\"%2$s\"}",
-				getMessage(), details);
-	}
+    public String toJson() {
+        return String.format("{\"error\":\"%1$s\",\"details\":\"%2$s\"}",
+                getMessage(), details);
+    }
 
-	public String toLog() {
-		return String.format("Reporting error: %1$s, details: %2$s",
-				getMessage(), details);
-	}
+    public String toLog() {
+        return String.format("Reporting error: %1$s, details: %2$s",
+                getMessage(), details);
+    }
 }
