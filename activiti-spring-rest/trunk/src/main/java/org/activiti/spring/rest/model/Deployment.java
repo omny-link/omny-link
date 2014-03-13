bdbf3c2f7423d9ed1ef2d50c8bc2a05010f60fdb
tstephenson724 <tstephenson724@41880c4b-a73f-416b-817f-a161b90437aa>
2014-03-13 21:23:33 +0000

    - Added signUpDate automatically - Added unordered findByExample - Restored transactionManager to the activiti rest library - Some more logging in the deployment controllers
    git-svn-id: https://svn-bigbpmcloud.forge.cloudbees.com/platform@70 41880c4b-a73f-416b-817f-a161b90437aa

--- activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/model/Deployment.java
+++ activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/model/Deployment.java
@@ -11,6 +11,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.activiti.engine.ProcessEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.equals.RooEquals;
@@ -24,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import flexjson.JSONSerializer;
 
+@SuppressWarnings("serial")
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
@@ -33,118 +36,126 @@ import flexjson.JSONSerializer;
 @Component
 public class Deployment {
 
-	private static ProcessEngine processEngine;
+    protected static final Logger LOGGER = LoggerFactory
+            .getLogger(Deployment.class);
+
+    private static ProcessEngine processEngine;
 
     /**
      */
-	@Id
+    @Id
     @Column(unique = true)
     private String id;
 
     /**
      */
-	private String name;
+    private String name;
 
-	/**
+    /**
      */
-	@Temporal(TemporalType.TIMESTAMP)
-	@DateTimeFormat(style = "M-")
-	private Date deploymentTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Date deploymentTime;
 
-	/**
+    /**
      */
-	private String category;
+    private String category;
 
-	/**
+    /**
      */
-	private String url;
+    private String url;
 
-	public Deployment() {
-		super();
-	}
+    public Deployment() {
+        super();
+    }
 
-	public Deployment(org.activiti.engine.repository.Deployment d) {
-		this();
-		setId(d.getId());
-		setName(d.getName());
-		setDeploymentTime(d.getDeploymentTime());
-		setCategory(d.getCategory());
-		// setUrl(d.getUrl());
-	}
+    public Deployment(org.activiti.engine.repository.Deployment d) {
+        this();
+        setId(d.getId());
+        setName(d.getName());
+        setDeploymentTime(d.getDeploymentTime());
+        setCategory(d.getCategory());
+        // setUrl(d.getUrl());
+    }
 
-	// Autowiring static fields is obviously dangerous, but should be ok in this
-	// case as PE is thread safe.
-	@Autowired(required = true)
-	public void setProcessEngine(ProcessEngine pe) {
-		Deployment.processEngine = pe;
-	}
+    // Autowiring static fields is obviously dangerous, but should be ok in this
+    // case as PE is thread safe.
+    @Autowired(required = true)
+    public void setProcessEngine(ProcessEngine pe) {
+        Deployment.processEngine = pe;
+    }
 
-	@Transactional
-	public void remove() {
-		processEngine.getRepositoryService().deleteDeployment(getId());
-	}
+    @Transactional
+    public void remove() {
+        LOGGER.warn(String.format("Deleting deployment %1$s", getId()));
+        processEngine.getRepositoryService().deleteDeployment(getId(), true);
 
-	public static long countDeployments() {
-		return processEngine.getRepositoryService().createDeploymentQuery()
-				.count();
-	}
+        long count = processEngine.getRepositoryService()
+                .createDeploymentQuery().deploymentId(getId()).count();
+        if (count > 0) {
+            throw new RuntimeException(String.format(
+                    "Failed to delete deployment with id ", getId()));
+        }
+    }
 
-	public static List<Deployment> findAllDeployments() {
-		return wrap(processEngine.getRepositoryService()
-				.createDeploymentQuery().list());
-	}
+    public static long countDeployments() {
+        return processEngine.getRepositoryService().createDeploymentQuery()
+                .count();
+    }
 
-	public static Deployment findDeployment(String id) {
-		return wrap(
-				processEngine.getRepositoryService().createDeploymentQuery()
-						.deploymentId(id).list()).get(0);
-	}
+    public static List<Deployment> findAllDeployments() {
+        return wrap(processEngine.getRepositoryService()
+                .createDeploymentQuery().list());
+    }
 
-	public static List<Deployment> findDeploymentEntries(int firstResult,
-			int maxResults) {
-		return wrap(processEngine.getRepositoryService()
-				.createDeploymentQuery()
-				.listPage(firstResult, maxResults));
-	}
+    public static Deployment findDeployment(String id) {
+        return wrap(
+                processEngine.getRepositoryService().createDeploymentQuery()
+                        .deploymentId(id).list()).get(0);
+    }
 
-	public static List<Deployment> findDeploymentEntries(int firstResult,
-			int maxResults,
-			String sortFieldName, String sortOrder) {
-		// TODO honour sort order
-		return wrap(processEngine.getRepositoryService()
-				.createDeploymentQuery()
-				.listPage(firstResult, maxResults));
-	}
+    public static List<Deployment> findDeploymentEntries(int firstResult,
+            int maxResults) {
+        return wrap(processEngine.getRepositoryService()
+                .createDeploymentQuery().listPage(firstResult, maxResults));
+    }
 
-	public static List<Deployment> findAllDeployments(String sortFieldName,
-			String sortOrder) {
-		System.out.println("pe: " + processEngine);
-		// TODO honour sort order
-		return wrap(processEngine.getRepositoryService()
-				.createDeploymentQuery().list());
-	}
+    public static List<Deployment> findDeploymentEntries(int firstResult,
+            int maxResults, String sortFieldName, String sortOrder) {
+        // TODO honour sort order
+        return wrap(processEngine.getRepositoryService()
+                .createDeploymentQuery().listPage(firstResult, maxResults));
+    }
 
-	private static List<Deployment> wrap(
-			final List<org.activiti.engine.repository.Deployment> list) {
-		ArrayList<Deployment> list2 = new ArrayList<Deployment>();
-		for (org.activiti.engine.repository.Deployment instance : list) {
-			list2.add(new Deployment(instance));
-		}
-		return list2;
-	}
+    public static List<Deployment> findAllDeployments(String sortFieldName,
+            String sortOrder) {
+        System.out.println("pe: " + processEngine);
+        // TODO honour sort order
+        return wrap(processEngine.getRepositoryService()
+                .createDeploymentQuery().list());
+    }
 
-	public static String toJsonArray(Collection<Deployment> collection) {
-		String[] fields = { "assignee", "createTime", "id", "name", "owner",
-				"parentDeploymentId", "priority", "processDefinitionId",
-				"suspended", "repositoryDefinitionKey" };
-		return toJsonArray(collection, fields);
-	}
+    private static List<Deployment> wrap(
+            final List<org.activiti.engine.repository.Deployment> list) {
+        ArrayList<Deployment> list2 = new ArrayList<Deployment>();
+        for (org.activiti.engine.repository.Deployment instance : list) {
+            list2.add(new Deployment(instance));
+        }
+        return list2;
+    }
 
-	public static String toJsonArray(Collection<Deployment> collection,
-			String[] fields) {
-		System.out.println("toJsonArray....");
-		return new JSONSerializer().exclude("*.class")
-				.exclude("*.processEngine").include(fields)
-				.serialize(collection);
-	}
+    public static String toJsonArray(Collection<Deployment> collection) {
+        String[] fields = { "assignee", "createTime", "id", "name", "owner",
+                "parentDeploymentId", "priority", "processDefinitionId",
+                "suspended", "repositoryDefinitionKey" };
+        return toJsonArray(collection, fields);
+    }
+
+    public static String toJsonArray(Collection<Deployment> collection,
+            String[] fields) {
+        System.out.println("toJsonArray....");
+        return new JSONSerializer().exclude("*.class")
+                .exclude("*.processEngine").include(fields)
+                .serialize(collection);
+    }
 }
