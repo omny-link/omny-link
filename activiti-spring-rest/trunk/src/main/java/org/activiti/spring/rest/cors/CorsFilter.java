957e0cb2a439f926772d8c440a3b6cc309b8b7b0
tstephenson724 <tstephenson724@41880c4b-a73f-416b-817f-a161b90437aa>
2014-03-20 18:25:32 +0000

    Fix to add 'expose' header so Location can be accessed by browser.
    git-svn-id: https://svn-bigbpmcloud.forge.cloudbees.com/platform@71 41880c4b-a73f-416b-817f-a161b90437aa

--- activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/cors/CorsFilter.java
+++ activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/cors/CorsFilter.java
@@ -23,116 +23,131 @@ import org.springframework.web.filter.OncePerRequestFilter;
  */
 public class CorsFilter extends OncePerRequestFilter {
 
-	private static final String CORS_PROPS = "/META-INF/cors.properties";
+    private static final String CORS_PROPS = "/META-INF/cors.properties";
 
-	protected static final String REQUEST_METHOD = "Access-Control-Request-Method";
+    protected static final String REQUEST_METHOD = "Access-Control-Request-Method";
 
-	protected static final String MAX_AGE = "Access-Control-Max-Age";
+    protected static final String MAX_AGE = "Access-Control-Max-Age";
 
-	protected static final String ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    protected static final String ALLOW_HEADERS = "Access-Control-Allow-Headers";
 
-	protected static final String ALLOW_METHODS = "Access-Control-Allow-Methods";
+    protected static final String ALLOW_METHODS = "Access-Control-Allow-Methods";
 
-	protected static final String ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    protected static final String ALLOW_ORIGIN = "Access-Control-Allow-Origin";
 
-	protected static final Logger LOGGER = LoggerFactory
-			.getLogger(CorsFilter.class);
+    protected static final Logger LOGGER = LoggerFactory
+            .getLogger(CorsFilter.class);
 
-	protected Set<String> allowedOrigins;
+    protected static final String EXPOSE_HEADERS = "Access-Control-Expose-Headers";
 
-	protected Set<String> getAllowedOrigins() throws IOException {
-		if (allowedOrigins == null) {
-			LOGGER.debug(String.format(
-					"Init allowed domains for CORS from %1$s...", CORS_PROPS));
-			Properties prop = new Properties();
+    protected Set<String> allowedOrigins;
 
-			InputStream in = CorsFilter.class.getResourceAsStream(CORS_PROPS);
-			prop.load(in);
-			in.close();
+    protected Set<String> getAllowedOrigins() throws IOException {
+        if (allowedOrigins == null) {
+            LOGGER.debug(String.format(
+                    "Init allowed domains for CORS from %1$s...", CORS_PROPS));
+            Properties prop = new Properties();
 
-			allowedOrigins = new HashSet<String>(Arrays.asList(prop
-					.getProperty("allowed.origins").split(",")));
-		}
-		LOGGER.debug("... allowed: " + allowedOrigins);
-		return allowedOrigins;
-	}
+            InputStream in = CorsFilter.class.getResourceAsStream(CORS_PROPS);
+            prop.load(in);
+            in.close();
 
-	public void setAllowedOrigins(String allowedOrigins) {
-		LOGGER.debug(String.format(
-				"Setting allowed domains for CORS to %1$s...", allowedOrigins));
+            allowedOrigins = new HashSet<String>(Arrays.asList(prop
+                    .getProperty("allowed.origins").split(",")));
+        }
+        LOGGER.debug("... allowed: " + allowedOrigins);
+        return allowedOrigins;
+    }
 
-		this.allowedOrigins = new HashSet<String>(Arrays.asList(allowedOrigins
-				.split(",")));
-	}
+    public void setAllowedOrigins(String allowedOrigins) {
+        LOGGER.debug(String.format(
+                "Setting allowed domains for CORS to %1$s...", allowedOrigins));
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request,
-			HttpServletResponse response, FilterChain filterChain)
-			throws ServletException, IOException {
-		String origin = request.getHeader("Origin");
-		if (origin == null) {
-			LOGGER.debug("CORS filter has nothing to do as Origin header is not specified.");
-		} else {
-			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug(String.format(
-						"Handling CORS request from: %1$s to %2$s ...", origin,
-						request.getMethod() + " "
-								+ request.getRequestURL().toString()));
-			}
+        this.allowedOrigins = new HashSet<String>(Arrays.asList(allowedOrigins
+                .split(",")));
+    }
 
-			String requestMethod = request.getHeader(REQUEST_METHOD);
-			if (requestMethod == null) {
-				LOGGER.warn(String
-						.format("... CORS handling from %2$s to %3$s abandoned due to missing header: %1$s. This can be normal if a CORS request is forwarded internally (as in an OpenId authentication).",
-								REQUEST_METHOD, origin, request.getMethod()
-										+ " "
-										+ request.getRequestURL().toString()));
-			} else if (getAllowedOrigins().contains(origin)) {
-				LOGGER.debug("... Cross-origin allowed.");
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+            HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String origin = request.getHeader("Origin");
+        if (origin == null) {
+            LOGGER.debug("CORS filter has nothing to do as Origin header is not specified.");
+        } else {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(String.format(
+                        "Handling CORS request from: %1$s to %2$s ...", origin,
+                        request.getMethod() + " "
+                                + request.getRequestURL().toString()));
+            }
 
-				// Spec calls for a single header and misconfiguration of
-				// filters can break this...
-				addOnlyOneHeader(response, ALLOW_ORIGIN, origin);
+            String requestMethod = request.getHeader(REQUEST_METHOD);
+            if (getAllowedOrigins().contains(origin)) {
+                LOGGER.debug("... Cross-origin allowed.");
 
-				// TODO Potentially add this here if needed.
-				// If the list of exposed headers is not empty add one or
-				// more
-				// Access-Control-Expose-Headers headers, with as values the
-				// header field names given in the list of exposed headers.
+                // Spec calls for a single header and misconfiguration of
+                // filters can break this...
+                addOnlyOneHeader(response, ALLOW_ORIGIN, origin);
 
-				// The following only applies to 'pre-flight' request
-				// Note this is the actual pre-flight method not the
-				// proposed
-				// CORS method that will follow
-				if ("OPTIONS".equals(request.getMethod())) {
-					// TODO parse Access-Control-Request-Headers
-					addOnlyOneHeader(response, MAX_AGE, "1800");
-					addOnlyOneHeader(response, ALLOW_METHODS,
-							"GET, POST, PUT, DELETE");
-					addOnlyOneHeader(
-							response,
-							ALLOW_HEADERS,
-							"Accept, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Access-Control-Request-Method, Authorization, Cache-Control, Connection, Content-Type, Host, Origin, Referer, User-Agent");
-				}
-			} else {
-				LOGGER.warn(String.format(
-						"... Cross origin disallowed from %1$s to %2$s",
-						origin, request.getMethod() + " "
-								+ request.getRequestURL().toString()));
-			}
-		}
+                // TODO Potentially add this here if needed.
+                // If the list of exposed headers is not empty add one or
+                // more
+                // Access-Control-Expose-Headers headers, with as values the
+                // header field names given in the list of exposed headers.
 
-		filterChain.doFilter(request, response);
-	}
+                // The following only applies to 'pre-flight' request
+                // Note this is the actual pre-flight method not the
+                // proposed
+                // CORS method that will follow
+                if ("OPTIONS".equals(request.getMethod())) {
+                    // TODO parse Access-Control-Request-Headers
+                    addOnlyOneHeader(response, MAX_AGE, "1800");
+                    addOnlyOneHeader(response, ALLOW_METHODS,
+                            "GET, POST, PUT, DELETE");
+                    addOnlyOneHeader(
+                            response,
+                            ALLOW_HEADERS,
+                            "Accept, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Access-Control-Request-Method, Authorization, Cache-Control, Connection, Content-Type, Host, Location, Origin, Referer, User-Agent");
+                    addOnlyOneHeader(response, EXPOSE_HEADERS, "Location");
+                } else {
+                    addOnlyOneHeader(response, EXPOSE_HEADERS, "Location");
+                }
+                if (LOGGER.isDebugEnabled()) { 
+                    LOGGER.debug(String.format(
+                            "contains CORS headers: expose-headers? %1$s",
+                            response.containsHeader(EXPOSE_HEADERS)));
+                }
 
-	protected void addOnlyOneHeader(HttpServletResponse response,
-			String header, String value) {
-		if (response.containsHeader(header)) {
-			LOGGER.warn(String
-					.format("Header %1$s already specified,  check only one CORS handler implemented",
-							header));
-		} else {
-			response.addHeader(header, value);
-		}
-	}
+                // TODO check spec, seems the request method is not sent at
+                // least for file:// urls check FF and chromium
+
+                // } else if (requestMethod == null) {
+                // LOGGER.warn(String
+                // .format("... CORS handling from %2$s to %3$s abandoned due to missing header: %1$s. This can be normal if a CORS request is forwarded internally (as in an OpenId authentication).",
+                // REQUEST_METHOD, origin, request.getMethod()
+                // + " "
+                // + request.getRequestURL().toString()));
+
+            } else {
+                LOGGER.warn(String.format(
+                        "... Cross origin disallowed from %1$s to %2$s",
+                        origin, request.getMethod() + " "
+                                + request.getRequestURL().toString()));
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    protected void addOnlyOneHeader(HttpServletResponse response,
+            String header, String value) {
+        if (response.containsHeader(header)) {
+            LOGGER.warn(String
+                    .format("Header %1$s already specified,  check only one CORS handler implemented",
+                            header));
+        } else {
+            response.addHeader(header, value);
+        }
+    }
 }
