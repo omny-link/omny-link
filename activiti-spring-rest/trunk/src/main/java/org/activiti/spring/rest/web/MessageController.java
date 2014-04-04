16b0694a8febb14c03e8f67a705c9af8729488bc
tstephenson724 <tstephenson724@41880c4b-a73f-416b-817f-a161b90437aa>
2014-04-04 12:19:52 +0000

    better logging and testing
    git-svn-id: https://svn-bigbpmcloud.forge.cloudbees.com/platform@79 41880c4b-a73f-416b-817f-a161b90437aa

--- activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/web/MessageController.java
+++ activiti-spring-rest/trunk/src/main/java/org/activiti/spring/rest/web/MessageController.java
@@ -42,182 +42,192 @@ import org.springframework.web.util.UriComponentsBuilder;
 // @RooWebScaffold(path = "msg", formBackingObject = Deployment.class)
 public class MessageController {
 
-	protected static final Logger LOGGER = LoggerFactory
-			.getLogger(MessageController.class);
+    protected static final Logger LOGGER = LoggerFactory
+            .getLogger(MessageController.class);
 
-	protected static ProcessEngine processEngine;
+    protected static ProcessEngine processEngine;
 
-	/**
-	 * Whether messages may be sent anonymously. Default: false.
-	 */
-	// TODO cannot figure how to get spring do property substitution
-	// @Value("${message.allowAnonymous}")
-	private String allowAnonymous = "true";
+    /**
+     * Whether messages may be sent anonymously. Default: false.
+     */
+    // TODO cannot figure how to get spring do property substitution
+    // @Value("${message.allowAnonymous}")
+    private String allowAnonymous = "true";
 
-	@Autowired
-	public void setProcessEngine(ProcessEngine pe) {
-		LOGGER.debug("Injected process engine into " + getClass().getName());
-		MessageController.processEngine = pe;
-	}
+    @Autowired
+    public void setProcessEngine(ProcessEngine pe) {
+        LOGGER.debug("Injected process engine into " + getClass().getName());
+        MessageController.processEngine = pe;
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{msgId}", headers = "Accept=application/json")
-	@ResponseBody
-	public final ResponseEntity<String> doInOutMep(
-			UriComponentsBuilder uriBuilder,
-			@PathVariable("msgId") String msgId,
-			@RequestParam("query") String jsonBody) {
-		// TODO need to look into why but the PathVariable is truncated at the
-		// last . so 'kp.foo' arrives as 'kp'. To work around this clients
-		// should send kp.foo.json or similar/
-		LOGGER.info(String.format("handling In-Out MEP to: %1$s, json: %2$s",
-				msgId, jsonBody));
-		String id = "";
-		Map<String, Object> vars = new HashMap<String, Object>();
-		HttpHeaders headers = null;
-		try {
-			ResponseEntity<String> response = handleMep(uriBuilder, msgId,
-					jsonBody, vars, 0);
-			if (response.getStatusCode().compareTo(HttpStatus.BAD_REQUEST) < 0) {
-				headers = response.getHeaders();
-				String locationHeader = response.getHeaders().get("Location")
-						.get(0);
-				LOGGER.debug("location: " + locationHeader);
-				id = locationHeader
-						.substring(locationHeader.lastIndexOf('/') + 1);
-				LOGGER.debug("id: " + id);
+    @RequestMapping(method = RequestMethod.GET, value = "/{msgId}", headers = "Accept=application/json")
+    @ResponseBody
+    public final ResponseEntity<String> doInOutMep(
+            UriComponentsBuilder uriBuilder,
+            @PathVariable("msgId") String msgId,
+            @RequestParam(required = false, value = "query") String json) {
+        // TODO need to look into why but the PathVariable is truncated at the
+        // last . so 'kp.foo' arrives as 'kp'. To work around this clients
+        // should send kp.foo.json or similar/
+        LOGGER.info(String.format("handling In-Out MEP to: %1$s, json: %2$s",
+                msgId, json));
+        String id = "";
+        Map<String, Object> vars = new HashMap<String, Object>();
+        HttpHeaders headers = null;
+        try {
+            ResponseEntity<String> response = handleMep(uriBuilder, msgId,
+                    json, vars, 0);
+            if (response.getStatusCode().compareTo(HttpStatus.BAD_REQUEST) < 0) {
+                headers = response.getHeaders();
+                String locationHeader = response.getHeaders().get("Location")
+                        .get(0);
+                LOGGER.debug("location: " + locationHeader);
+                id = locationHeader
+                        .substring(locationHeader.lastIndexOf('/') + 1);
+                LOGGER.debug("id: " + id);
 
-				String msg = (String) processEngine.getRuntimeService()
-						.getVariable(id, msgId);
-				LOGGER.debug("msg: " + msg);
-				return new ResponseEntity(msg, headers, HttpStatus.OK);
-			} else {
-				return response;
-			}
-		} catch (ActivitiObjectNotFoundException e) {
-			// assume process is closed
-			HistoricVariableInstance instance = processEngine
-					.getHistoryService().createHistoricVariableInstanceQuery()
-					.processInstanceId(id).variableName(msgId).singleResult();
-			String msg = (String) instance.getValue();
-			LOGGER.debug("msg: " + msg);
-			return new ResponseEntity(msg, headers, HttpStatus.OK);
-		} catch (Exception e) {
-			ReportableException e2 = new ReportableException(e.getClass()
-					.getName() + ":" + e.getMessage(), e);
-			return new ResponseEntity(e2.toJson(), headers,
-					HttpStatus.BAD_REQUEST);
-		}
-	}
+                String msg = (String) processEngine.getRuntimeService()
+                        .getVariable(id, msgId);
+                LOGGER.debug("msg: " + msg);
+                return new ResponseEntity(msg, headers, HttpStatus.OK);
+            } else {
+                return response;
+            }
+        } catch (ActivitiObjectNotFoundException e) {
+            // assume process is closed
+            LOGGER.debug(String
+                    .format("Activiti object not found: %1$s. Assume process is ended...",
+                            e.getMessage()));
+            HistoricVariableInstance instance = processEngine
+                    .getHistoryService().createHistoricVariableInstanceQuery()
+                    .processInstanceId(id).variableName(msgId).singleResult();
+            String msg = (String) instance.getValue();
+            LOGGER.debug("msg: " + msg);
+            return new ResponseEntity(msg, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            ReportableException e2 = new ReportableException(e.getClass()
+                    .getName() + ":" + e.getMessage(), e);
+            return new ResponseEntity(e2.toJson(), headers,
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
 
-	@RequestMapping(method = RequestMethod.POST, value = "/{msgId}", headers = "Accept=application/json")
-	@ResponseBody
-	public ResponseEntity<String> doInOnlyMep(UriComponentsBuilder uriBuilder,
-			@PathVariable("msgId") String msgId, @RequestParam String json) {
-		long start = System.currentTimeMillis();
-		LOGGER.info("handling In-Only MEP: " + msgId + ", json:" + json);
+    @RequestMapping(method = RequestMethod.POST, value = "/{msgId}", headers = "Accept=application/json")
+    @ResponseBody
+    public ResponseEntity<String> doInOnlyMep(UriComponentsBuilder uriBuilder,
+            @PathVariable("msgId") String msgId, @RequestParam String json) {
+        long start = System.currentTimeMillis();
+        LOGGER.info("handling In-Only MEP: " + msgId + ", json:" + json);
 
-		Map<String, Object> vars = new HashMap<String, Object>();
-		ResponseEntity<String> response = handleMep(uriBuilder, msgId, json,
-				vars, 0);
+        Map<String, Object> vars = new HashMap<String, Object>();
+        ResponseEntity<String> response = handleMep(uriBuilder, msgId, json,
+                vars, 0);
 
-		LOGGER.debug(String.format("doInOnlyMep took: %1$s ms",
-				(System.currentTimeMillis() - start)));
-		return response;
-	}
+        LOGGER.debug(String.format("doInOnlyMep took: %1$s ms",
+                (System.currentTimeMillis() - start)));
+        return response;
+    }
 
-	protected ResponseEntity<String> handleMep(UriComponentsBuilder uriBuilder,
-			String msgId, String jsonBody, Map<String, Object> vars, int retry) {
-		if (SecurityContextHolder.getContext().getAuthentication() == null
-				|| "anonymousUser".equals(SecurityContextHolder.getContext()
-						.getAuthentication().getName())) {
-			LOGGER.debug("allow Anonymous is set to " + allowAnonymous);
-			if (Boolean.valueOf(allowAnonymous)) {
-				LOGGER.warn("No user associated with this message, this may result in errors if the process author expected a username.");
-			} else {
-				ReportableException e = new ReportableException(
-						"Please ensure you are logged in before sending messages");
-				return new ResponseEntity(e.toJson(), HttpStatus.UNAUTHORIZED);
-			}
-		}
-		String username = SecurityContextHolder.getContext()
-				.getAuthentication().getName();
-		LOGGER.debug(" ... from (getName) " + username);
+    protected ResponseEntity<String> handleMep(UriComponentsBuilder uriBuilder,
+            String msgId, String jsonBody, Map<String, Object> vars, int retry) {
+        if (SecurityContextHolder.getContext().getAuthentication() == null
+                || "anonymousUser".equals(SecurityContextHolder.getContext()
+                        .getAuthentication().getName())) {
+            LOGGER.debug("allow Anonymous is set to " + allowAnonymous);
+            if (Boolean.valueOf(allowAnonymous)) {
+                LOGGER.warn("No user associated with this message, this may result in errors if the process author expected a username.");
+            } else {
+                ReportableException e = new ReportableException(
+                        "Please ensure you are logged in before sending messages");
+                return new ResponseEntity(e.toJson(), HttpStatus.UNAUTHORIZED);
+            }
+        }
+        String username = SecurityContextHolder.getContext()
+                .getAuthentication().getName();
+        LOGGER.debug(" ... from (getName) " + username);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/json");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
 
-		try {
-			String bizKey = msgId + " - " + new Date().getTime();
-			Authentication.setAuthenticatedUserId(username);
-			vars.put("messageName", msgId);
-			vars.put(msgId, jsonBody);
-			// TODO deprecate query param?
-			vars.put("query", jsonBody);
+        try {
+            String bizKey = msgId + " - " + new Date().getTime();
+            Authentication.setAuthenticatedUserId(username);
+            vars.put("messageName", msgId);
+            vars.put(msgId, jsonBody);
+            // TODO deprecate query param?
+            vars.put("query", jsonBody);
 
-			vars.put("initiator", username);
-			LOGGER.debug(String.format("vars: %1$s", vars));
-			ProcessInstance instance = processEngine.getRuntimeService()
-					.startProcessInstanceByMessage(msgId, bizKey, vars);
+            vars.put("initiator", username);
+            LOGGER.debug(String.format("vars: %1$s", vars));
+            ProcessInstance instance = processEngine.getRuntimeService()
+                    .startProcessInstanceByMessage(msgId, bizKey, vars);
 
-			List<ProcessInstance> list = processEngine.getRuntimeService()
-					.createProcessInstanceQuery()
-					.processInstanceId(instance.getId()).list();
-			RequestMapping a = ProcessInstanceController.class
-					.getAnnotation(RequestMapping.class);
-			if (list.size() == 0) {
-				// already ended
-				// a =
-				// HistoryController.class.getAnnotation(RequestMapping.class);
-			}
-			headers.add("Location",
-					uriBuilder.path(a.value()[0] + "/" + instance.getId())
-							.build().toUriString());
-			return new ResponseEntity<String>(headers, HttpStatus.CREATED);
-		} catch (CannotCreateTransactionException e) {
-			LOGGER.error(e.getClass().getName() + ":" + e.getMessage(), e);
-			if (e.getCause() instanceof com.mysql.jdbc.exceptions.jdbc4.CommunicationsException
-					&& retry < 3) {
-				LOGGER.error("Confirmed timeout exception, retrying...");
-				return handleMep(uriBuilder, msgId, jsonBody, vars, ++retry);
-			} else {
-				ReportableException e2 = new ReportableException(
-						"Cause is not timeout or retries exceeds limit", e);
-				return new ResponseEntity<String>(e2.toJson(), headers,
-						HttpStatus.SERVICE_UNAVAILABLE);
-			}
-		} catch (ActivitiException e) {
-			ReportableException e2 = null;
-			ConstraintViolationException cve = isCausedBy(e,
-					ConstraintViolationException.class.getName());
-			if (cve != null) {
-				e2 = new ReportableException(cve);
-				return new ResponseEntity<String>(e2.toJson(), headers,
-						HttpStatus.BAD_REQUEST);
-			} else {
-				e2 = new ReportableException(e.getClass().getName() + ":"
-						+ e.getMessage(), e);
-				return new ResponseEntity<String>(e2.toJson(), headers,
-						HttpStatus.SERVICE_UNAVAILABLE);
-			}
-		} catch (Exception e) {
-			ReportableException e2 = new ReportableException(e.getClass()
-					.getName() + ":" + e.getMessage(), e);
-			return new ResponseEntity<String>(e2.toJson(), headers,
-					HttpStatus.BAD_REQUEST);
-		} finally {
-			Authentication.setAuthenticatedUserId(null);
-		}
-	}
+            List<ProcessInstance> list = processEngine.getRuntimeService()
+                    .createProcessInstanceQuery()
+                    .processInstanceId(instance.getId()).list();
+            RequestMapping a = ProcessInstanceController.class
+                    .getAnnotation(RequestMapping.class);
+            if (list.size() == 0) {
+                // already ended
+                // a =
+                // HistoryController.class.getAnnotation(RequestMapping.class);
+            }
+            headers.add("Location",
+                    uriBuilder.path(a.value()[0] + "/" + instance.getId())
+                            .build().toUriString());
+            return new ResponseEntity<String>(headers, HttpStatus.CREATED);
+        } catch (CannotCreateTransactionException e) {
+            LOGGER.error(e.getClass().getName() + ":" + e.getMessage(), e);
+            if (e.getCause() instanceof com.mysql.jdbc.exceptions.jdbc4.CommunicationsException
+                    && retry < 3) {
+                LOGGER.error("Confirmed timeout exception, retrying...");
+                return handleMep(uriBuilder, msgId, jsonBody, vars, ++retry);
+            } else {
+                ReportableException e2 = new ReportableException(
+                        "Cause is not timeout or retries exceeds limit", e);
+                LOGGER.error("  retries: " + retry);
+                LOGGER.error("  e.getCause() instanceof com.mysql.jdbc.exceptions.jdbc4.CommunicationsException"
+                        + (e.getCause() instanceof com.mysql.jdbc.exceptions.jdbc4.CommunicationsException));
+                LOGGER.error(e.getMessage(), e);
+                return new ResponseEntity<String>(e2.toJson(), headers,
+                        HttpStatus.SERVICE_UNAVAILABLE);
+            }
+        } catch (ActivitiException e) {
+            LOGGER.error(e.getMessage(), e);
+            ReportableException e2 = null;
+            ConstraintViolationException cve = isCausedBy(e,
+                    ConstraintViolationException.class.getName());
+            if (cve != null) {
+                e2 = new ReportableException(cve);
+                return new ResponseEntity<String>(e2.toJson(), headers,
+                        HttpStatus.BAD_REQUEST);
+            } else {
+                e2 = new ReportableException(e.getClass().getName() + ":"
+                        + e.getMessage(), e);
+                return new ResponseEntity<String>(e2.toJson(), headers,
+                        HttpStatus.SERVICE_UNAVAILABLE);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            ReportableException e2 = new ReportableException(e.getClass()
+                    .getName() + ":" + e.getMessage(), e);
+            return new ResponseEntity<String>(e2.toJson(), headers,
+                    HttpStatus.BAD_REQUEST);
+        } finally {
+            Authentication.setAuthenticatedUserId(null);
+        }
+    }
 
-	private ConstraintViolationException isCausedBy(Throwable e,
-			String className) {
-		if (e.getCause().getClass().getName().equals(className)) {
-			return (ConstraintViolationException) e.getCause();
-		} else if (e.getCause() != null) {
-			return isCausedBy(e.getCause(), className);
-		} else {
-			return null;
-		}
-	}
+    private ConstraintViolationException isCausedBy(Throwable e,
+            String className) {
+        if (e.getCause().getClass().getName().equals(className)) {
+            return (ConstraintViolationException) e.getCause();
+        } else if (e.getCause() != null) {
+            return isCausedBy(e.getCause(), className);
+        } else {
+            LOGGER.error(e.getMessage(), e);
+            return null;
+        }
+    }
 
 }
