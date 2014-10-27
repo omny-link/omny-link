921dbb92a7ec5726cedf750968c05a360e99d1de
Tim Stephenson <tim@knowprocess.com>
2014-10-27 11:02:14 +0000

    Release 0.7: Add post publication event Add subscription via ajax including setting user meta fields as well as creating account

--- wp_workflow/trunk/emails/templates.php
+++ wp_workflow/trunk/emails/templates.php
@@ -1,4 +1,5 @@
 <?php
+define('BASE_URL','http://wp.knowprocess.com/wp-content/plugins/syncapt/emails/');
 $iterator = new GlobIterator("*.html");
 $filelist = array();
 foreach($iterator as $entry) {
@@ -6,9 +7,9 @@ foreach($iterator as $entry) {
     //$filelist[] = substr($entry->getFilename(),1,5);
 }
 $i = 0;
-echo "[";
+echo "[\" --- please select template ---\",";
 foreach($filelist as $entry) {
-    echo "\"".$entry."\"";
+    echo "\"".BASE_URL.$entry."\"";
     $i++;
     if ($i < count($filelist)) echo ",";    
 }
