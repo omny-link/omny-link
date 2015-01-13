02b047ad43bdf2eadc50117e9abf6a610e8e61a8
Tim Stephenson <tim@knowprocess.com>
2015-01-13 17:09:57 +0000

    0.9.1 release of syncapt plugin

--- wp_workflow/trunk/includes/shortcodes.php
+++ wp_workflow/trunk/includes/shortcodes.php
@@ -29,12 +29,16 @@
   function p_form_shortcode( $atts ) {
     $a = shortcode_atts( array(
       'id' => '',
+      'msg_name' => '',
+      'redirect_to' => '/',
       'title' => '',
     ), $atts );
     $syncapt_options = new SyncaptOptions();
     ob_start();
 
     $form = get_post($a['id']);
+    if ($a['msg_name']=='') $a['msg_name'] = $form->post_name;
+
     $form_content = $form->post_content;
     $form_content = apply_filters('the_content', $form_content);
     $form_content = str_replace(']]>', ']]&gt;', $form_content);
@@ -44,7 +48,7 @@
 
     $temp_content .= $form_content;
 
-    $temp_content .= '<button data-p-action="$p.sendMessage(\'inOnly\', \''.$syncapt_options->get_message_namespace().'/'.$form->post_name.'\', $p.'.str_replace('-','_',$form->post_name).')" form="'.$form->post_name.'" type="button">Submit</button>';
+    $temp_content .= '<button data-p-action="if (document.getElementById(\''.$form->post_name.'\').checkValidity()) $p.sendMessage(\'inOnly\', \''.$syncapt_options->get_message_namespace().'.'.$a['msg_name'].'\', $p.'.str_replace('-','_',$form->post_name).',\''.$a['redirect_to'].'\'); else $p.showFormError(\''.$form->post_name.'\',\'Please correct form errors\');" form="'.$form->post_name.'" type="button">Submit</button>';
     $temp_content .= '</form>';
 
     ob_end_clean();
@@ -73,5 +77,4 @@
     return ob_get_clean();
   }
   add_shortcode( 'p_tasks', 'p_tasks_shortcode' );
-
 ?>
