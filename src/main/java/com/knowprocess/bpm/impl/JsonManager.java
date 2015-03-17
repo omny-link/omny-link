52c800b2e00ace86c980f32732599128890ab352
Tim Stephenson <tim@knowprocess.com>
2015-03-17 20:10:34 +0000

    clean up logging

--- src/main/java/com/knowprocess/bpm/impl/JsonManager.java
+++ src/main/java/com/knowprocess/bpm/impl/JsonManager.java
@@ -8,9 +8,18 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonWriter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JsonManager {
 
+    protected static final Logger LOGGER = LoggerFactory
+            .getLogger(JsonManager.class);
+
     public JsonObject toObject(String json) {
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Parsing: " + json);
+        }
         JsonReader jsonReader = Json.createReader(new StringReader(json));
         return jsonReader.readObject();
     }
