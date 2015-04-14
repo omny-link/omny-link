e171bc038ddb0dc2bc523820aa2ce7dc92aff9dc
Tim Stephenson <tim@knowprocess.com>
2015-04-14 01:05:01 +0100

    UI updates included in v0.4, largely for gardenatics

---
+++ cust-mgmt/src/main/resources/static/js/string-functions-0.1.0.js
@@ -0,0 +1,26 @@
+String.prototype.toLabel = function() {
+  return this.replace(/_/g, ' ').toLeadingCaps();
+};
+
+String.prototype.toLeadingCaps = function() {
+  if (this == undefined) return this
+  return this.replace(/(&)?([a-z])([a-z]{2,})(;)?/ig, function(all, prefix, letter, word, suffix) {
+    if (prefix && suffix) {
+      return all;
+    }
+    return letter.toUpperCase() + word.toLowerCase();
+  });
+}
+
+String.prototype.formatNumber = function(thousandsSeparator) {
+  // reverse string and insert separator every 3 digits
+  var chars = this.split('');
+  return chars.reverse().reduce(function(previousValue, currentValue, idx, arr) {
+    if (idx % 3 == 0 && idx != 0) { 
+      return previousValue + thousandsSeparator + currentValue;
+    } else { 
+      return previousValue + currentValue;
+    }
+  }, '') // need to init string
+  .split('').reverse().join(''); // don't forget to restore number to correct order!
+}
