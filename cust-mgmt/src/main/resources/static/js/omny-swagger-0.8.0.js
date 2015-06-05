a7e5a2c31937190a5a489ae2b4efd5d80c19aa81
Tim Stephenson <tim@knowprocess.com>
2015-06-05 13:11:29 +0100

    #58 Apply Omny branding to Swagger UI

---
+++ cust-mgmt/src/main/resources/static/js/omny-swagger-0.8.0.js
@@ -0,0 +1,15 @@
+var os = new OmnySwagger();
+
+function OmnySwagger() { 
+  this.onSwaggerComplete = function() { 
+    console.log('onSwaggerComplete');
+    
+    $('.info_title').empty().append('API Documentation');
+    $('.info_description').remove();
+    $('.info_name').empty().append('Created by Tim Stephenson (tim at omny.link)');
+    $('.info_license').empty().append('<a href="http://www.gnu.org/licenses/lgpl-3.0.en.html">LGPL license v3.0</a>');
+    $('#header').css('background-color','white');
+    $('#logo').remove();
+    $('#header .swagger-ui-wrap').prepend('<a href="//api.omny.link"><img src="/images/omny-logo.png" style="height: 70px" alt="logo"></a>');
+  }
+}
