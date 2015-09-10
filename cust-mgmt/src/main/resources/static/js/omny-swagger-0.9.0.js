0db1362061318d6ff875920f1e6948711ac2c1f7
Tim Stephenson <tim@knowprocess.com>
2015-09-10 14:33:53 +0100

    apply branding to embedded refactor onedecision and swagger ui

--- cust-mgmt/src/main/resources/static/js/omny-swagger-0.9.0.js
+++ cust-mgmt/src/main/resources/static/js/omny-swagger-0.9.0.js
@@ -10,6 +10,7 @@ function OmnySwagger() {
     $('.info_license').empty().append('<a href="http://www.gnu.org/licenses/lgpl-3.0.en.html">LGPL license v3.0</a>');
     $('#header').css('background-color','white');
     $('#logo').remove();
-    $('#header .swagger-ui-wrap').prepend('<a href="//api.omny.link"><img src="/images/omny-logo.png" style="height: 70px" alt="logo"></a>');
+    $('#header .swagger-ui-wrap .navbar-brand').remove();
+    $('#header .swagger-ui-wrap').prepend('<a class="nav navbar-brand" href="//api.omny.link"><img src="/images/omny-logo.png" style="height: 70px" alt="logo"></a>');
   }
 }
