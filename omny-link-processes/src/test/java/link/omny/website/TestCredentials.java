37dd6494f5f173a82645f9ed45752d78250fdfaf
Tim Stephenson <tim@knowprocess.com>
2015-11-11 17:53:23 +0000

    support for posting tweets, tested at unit and process level

--- omny-link-processes/src/test/java/link/omny/website/TestCredentials.java
+++ omny-link-processes/src/test/java/link/omny/website/TestCredentials.java
@@ -16,6 +16,24 @@ public class TestCredentials {
         idSvc.saveUser(botUser);
         idSvc.setUserInfo(BOT_USERNAME, "cust-mgmt-secret", BOT_PWD);
         idSvc.setUserInfo(BOT_USERNAME, "cust-mgmt-url", CUST_MGMT_URL);
+
+        if (System.getProperty("consumerKey") != null) {
+            idSvc.setUserInfo(BOT_USERNAME, "twitter-consumer-key",
+                    System.getProperty("consumerKey"));
+        }
+        if (System.getProperty("consumerSecret") != null) {
+            idSvc.setUserInfo(BOT_USERNAME, "twitter-consumer-secret",
+                    System.getProperty("consumerSecret"));
+        }
+        if (System.getProperty("accessToken") != null) {
+            idSvc.setUserInfo(BOT_USERNAME, "twitter-access-token",
+                    System.getProperty("accessToken"));
+        }
+        if (System.getProperty("accessSecret") != null) {
+            idSvc.setUserInfo(BOT_USERNAME, "twitter-access-secret",
+                    System.getProperty("accessSecret"));
+        }
+
     }
 
     public static void removeBot(IdentityService idSvc, String tenantId) {
