4d1c05989caa21d9d3bd876cfc49c267a019fecb
Tim Stephenson <tim@knowprocess.com>
2016-01-13 21:11:04 +0000

    #87 prototype of 90 day moving window report for various metrics

---
+++ acct-mgmt/src/main/java/link/omny/acctmgmt/repositories/MetricRepository.java
@@ -0,0 +1,24 @@
+package link.omny.acctmgmt.repositories;
+
+import java.util.Date;
+import java.util.List;
+
+import link.omny.acctmgmt.model.Metric;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "/metrics")
+public interface MetricRepository extends CrudRepository<Metric, String> {
+
+    @Query("SELECT c FROM Metric c WHERE c.tenantId = :tenantId AND c.occurred >= :since ORDER BY c.occurred DESC")
+    List<Metric> findAllSinceDateForTenant(@Param("since") Date date,
+            @Param("tenantId") String tenantId);
+
+    @Query("SELECT c FROM Metric c WHERE c.name = :name AND c.tenantId = :tenantId AND c.occurred >= :since ORDER BY c.occurred DESC")
+    List<Metric> findByNameSinceDateForTenant(@Param("name") String name,
+            @Param("since") Date date, @Param("tenantId") String tenantId);
+
+}
