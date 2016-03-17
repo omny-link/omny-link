5456e2d0a9e01bd15558a1ad78fbc766e3f539ef
Tim Stephenson <tim@knowprocess.com>
2016-03-17 12:02:00 +0000

    Added system config

--- acct-mgmt/src/main/java/link/omny/acctmgmt/web/SystemConfigController.java
+++ acct-mgmt/src/main/java/link/omny/acctmgmt/web/SystemConfigController.java
@@ -1,5 +1,6 @@
 package link.omny.acctmgmt.web;
 
+import link.omny.acctmgmt.model.SystemConfig;
 import link.omny.acctmgmt.model.TenantConfig;
 import link.omny.acctmgmt.repositories.TenantConfigRepository;
 
@@ -13,16 +14,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/configuration")
-public class TenantConfigController {
+public class SystemConfigController {
 
     protected static final Logger LOGGER = LoggerFactory
-            .getLogger(TenantConfigController.class);
+            .getLogger(SystemConfigController.class);
+
+    @Autowired
+    private SystemConfig systemConfig;
 
     @Autowired
     protected TenantConfigRepository tenantRepo;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET, headers = "Accept=application/json")
-    public @ResponseBody Iterable<TenantConfig> showAllJson() {
+    @RequestMapping(method = RequestMethod.GET)
+    public final @ResponseBody SystemConfig getSystemConfig() {
+        LOGGER.info("getSystemConfig");
+
+        return systemConfig;
+    }
+
+    @RequestMapping(value = "/tenants", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody Iterable<TenantConfig> showAllTenants() {
         LOGGER.info(String.format("showAllJson"));
 
         return tenantRepo.findAll();
