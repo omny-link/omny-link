f4c70d3f0d53cfd64086bd0fe29c34ad11b2285f
Tim Stephenson <tim@knowprocess.com>
2016-06-10 19:06:41 +0100

    #177 IE doesn't like dates

--- work-mgmt/src/main/resources/static/js/login-1.0.0.js
+++ work-mgmt/src/main/resources/static/js/login-1.0.0.js
@@ -445,3 +445,17 @@ function selectElementContents(el) {
   sel.removeAllRanges();
   sel.addRange(range);
 }
+
+/******************************** Polyfills **********************************/
+// ref https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
