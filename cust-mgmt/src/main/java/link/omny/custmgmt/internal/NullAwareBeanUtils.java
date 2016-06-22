5d3b9b91d75b3dc3414d64d8a3c11d8af2d322d8
Tim Stephenson <tim@trakeo.com>
2016-06-22 19:04:10 +0100

    #166 selective update of non null only contact props

---
+++ cust-mgmt/src/main/java/link/omny/custmgmt/internal/NullAwareBeanUtils.java
@@ -0,0 +1,47 @@
+package link.omny.custmgmt.internal;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class NullAwareBeanUtils {
+
+	public static Set<String> getNullPropertyNames(Object source) {
+		final BeanWrapper src = new BeanWrapperImpl(source);
+		PropertyDescriptor[] pds = src.getPropertyDescriptors();
+	
+		Set<String> emptyNames = new HashSet<String>();
+		for (PropertyDescriptor pd : pds) {
+            if (pd.getReadMethod() == null
+                    || src.getPropertyValue(pd.getName()) == null) {
+				emptyNames.add(pd.getName());
+            }
+		}
+		return emptyNames;
+	}
+
+	public static void copyNonNullProperties(Object srcBean,
+			Object trgtBean) {
+		Set<String> ignoreNames = getNullPropertyNames(srcBean);
+		String[] ignoreProperties = new String[ignoreNames.size()];
+		BeanUtils.copyProperties(srcBean, trgtBean,
+				ignoreNames.toArray(ignoreProperties));
+	}
+
+	public static void copyNonNullProperties(Object srcBean,
+			Object trgtBean,
+			String... additionalIgnores) {
+		Set<String> ignoreNames = getNullPropertyNames(srcBean);
+		for (String prop : additionalIgnores) {
+			ignoreNames.add(prop);
+		}
+		String[] ignoreProperties = new String[ignoreNames.size()];
+		BeanUtils.copyProperties(srcBean, trgtBean,
+				ignoreNames.toArray(ignoreProperties));
+	}
+
+}
