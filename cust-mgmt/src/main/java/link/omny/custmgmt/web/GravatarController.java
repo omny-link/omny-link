14f5679e0fb2e02c76aeaf0cd0e7ec0574d234ce
Tim Stephenson <tim@knowprocess.com>
2016-07-04 19:34:34 +0100

    #199 fix security for gravatars

--- cust-mgmt/src/main/java/link/omny/custmgmt/web/GravatarController.java
+++ cust-mgmt/src/main/java/link/omny/custmgmt/web/GravatarController.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,18 +35,23 @@ public class GravatarController {
      * Return a gravatar based on contact's initials if known email hash or
      * nothing.
      */
-    @RequestMapping(value = "/{emailHash}", method = RequestMethod.GET, produces=MediaType.IMAGE_PNG_VALUE)
+    @RequestMapping(value = "/{emailHash}.png", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
     public @ResponseBody void getGravatar(
             @PathVariable("tenantId") String tenantId,
             @PathVariable("emailHash") String emailHash,
             HttpServletResponse response) {
 
-        List<Contact> contacts = contactRepo.findByEmailHash(
-                emailHash, tenantId);
+        List<Contact> contacts = contactRepo.findByEmailHash(emailHash,
+                tenantId);
 
         try {
-            avatarSvc.create(contacts.get(0).initials(),
-                    response.getOutputStream());
+            if (contacts == null || contacts.size() < 1) {
+                avatarSvc.create("N/A",
+                        response.getOutputStream());
+            } else {
+                avatarSvc.create(contacts.get(0).initials(),
+                        response.getOutputStream());
+            }
         } catch (Exception e) {
             LOGGER.error(String.format("Unable to create avatar for %1$s",
                     emailHash), e);
