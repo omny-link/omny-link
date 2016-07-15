7df200d5db57b8d4563b232a02064a28885d4f54
Tim Stephenson <tim@knowprocess.com>
2016-07-15 14:22:16 +0100

    #221 Circular references between UserRecord and UserInfo

--- work-mgmt/src/main/java/com/knowprocess/bpm/model/UserInfo.java
+++ work-mgmt/src/main/java/com/knowprocess/bpm/model/UserInfo.java
@@ -3,15 +3,12 @@ package com.knowprocess.bpm.model;
 import java.io.Serializable;
 
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import org.springframework.stereotype.Component;
 
 @Data
-@EqualsAndHashCode(exclude = { "userRecord" })
 @Component
 public class UserInfo implements Serializable {
 
@@ -19,56 +16,25 @@ public class UserInfo implements Serializable {
 
     /**
      */
-	@Id
-	private String id;
+    @Id
+    private String id;
 
-	/**
+    /**
      */
-	private String key;
+    private String key;
 
-	/**
+    /**
      */
-	private String value;
+    private String value;
 
-	/**
-     */
-	@ManyToOne
-	private UserRecord userRecord;
+    public UserInfo() {
+        super();
+    }
 
-	public UserInfo() {
-		super();
-	}
+    public UserInfo(String key, String value) {
+        this();
+        setKey(key);
+        setValue(value);
+    }
 
-	public UserInfo(UserRecord userRecord, String key, String value) {
-		this();
-		this.key = key;
-		this.value = value;
-		this.userRecord = userRecord;
-	}
-
-	public UserInfo(String key, String value) {
-		this();
-		setKey(key);
-		setValue(value);
-	}
-
-//	public String toJson() {
-//		return String
-//				.format("{\"id\":\"%1$s\",\"key\":\"%1$2\",\"value\":\"%3$s\",\"%4$s\":\"%5$s\"}",
-//						getId(), getKey(), getValue(), getKey(), getValue());
-//	}
-//
-//	public static String toJsonArray(Collection<UserInfo> collection) {
-//		StringBuffer sb = new StringBuffer("[");
-//		for (Iterator<UserInfo> iterator = collection.iterator(); iterator
-//				.hasNext();) {
-//			UserInfo userInfo = iterator.next();
-//			sb.append(userInfo.toJson());
-//			if (iterator.hasNext()) {
-//				sb.append(",");
-//			}
-//		}
-//		sb.append("]");
-//		return sb.toString();
-//	}
 }
