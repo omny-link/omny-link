fdce9e8c4bceef25598af89391d5b1c56ec573f2
Tim Stephenson <tim@knowprocess.com>
2016-10-15 16:48:48 +0100

    #239 Ticket system

--- work-mgmt/src/main/resources/static/js/support-1.0.0.js
+++ work-mgmt/src/main/resources/static/js/support-1.0.0.js
@@ -2,22 +2,26 @@
 var hd = new HelpDesk();
 
 function HelpDesk() {
-  this.submit = function(obj) {
+  this.createTicket = function() {
+    console.info('createTicket');
+    $('#ticketModal').modal({});
+  }
+  this.submit = function() {
     var c = document.getElementById('hdScreenshot');
-    var canvasData = c.toDataURL("image/png");
 //    c.toBlob(function(blob) {
 //      console.log('have blob, submitting...');
       $.ajax({
         url: ractive.getServer()+'/omny/helpdesk',
         type: 'POST',
         contentType: 'application/upload',
-        data: JSON.stringify({ image:canvasData, entity: obj }),
+        data: JSON.stringify({ message: $('#ticketForm #what').val(), image:c.toDataURL("image/png") }),
         success: completeHandler = function(data,textStatus,jqXHR) {
           console.log('response code: '+ jqXHR.status+', Location: '+jqXHR.getResponseHeader('Location'));
-          ractive.showMessage('Submitted screenshot');
+          ractive.showMessage('Submitted ticket');
         },
       });
 //    });
+    $('#ticketModal').modal('hide');
   };
   this.takeScreenshot = function(obj) {
     html2canvas(document.body).then(function(canvas) {
@@ -31,7 +35,7 @@ function HelpDesk() {
       canvas.setAttribute('style', 'display:none');
       document.body.appendChild(canvas);
       
-      hd.submit(obj);
+      hd.createTicket();
     });
   }
 }
