762eed373e453998edcd9ae1635eb120abaf635b
Tim Stephenson <tim@knowprocess.com>
2016-12-02 10:42:51 +0000

    #349 added the cc functionality to SendEmail process

--- service-tasks/src/main/java/com/knowprocess/core/internal/BaseUserAwareTask.java
+++ service-tasks/src/main/java/com/knowprocess/core/internal/BaseUserAwareTask.java
@@ -2,16 +2,11 @@ package com.knowprocess.core.internal;
 
 import javax.annotation.Nonnull;
 
-import org.activiti.engine.ActivitiException;
-import org.activiti.engine.ActivitiObjectNotFoundException;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.delegate.JavaDelegate;
-import org.activiti.engine.identity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.knowprocess.exceptions.UserInfoNotFoundException;
 
 /**
  * Base class capable of looking up user settings such as for external accounts.
@@ -21,36 +16,20 @@ public abstract class BaseUserAwareTask implements JavaDelegate {
     public static final Logger LOGGER = LoggerFactory
             .getLogger(BaseUserAwareTask.class);
 
+    private UserInfoHelper userInfoHelper;
+
     protected Expression resourceUsername;
     protected Expression resourcePassword;
 
-    protected String lookup(DelegateExecution execution, @Nonnull String usr, @Nonnull Expression expr) {
-        String s = (String) expr.getValue(execution);
-        try {
-            if (s.startsWith("userInfo('")) {
-                String key = s.substring("userInfo(".length(), s.indexOf(')'));
-                if (key.startsWith("'") || key.startsWith("\"")) {
-                    key = key.substring(1, key.length() - 1);
-                }
-                String val = execution.getEngineServices().getIdentityService()
-                        .getUserInfo(usr, key);
-                if (val == null) {
-                    throw new ActivitiObjectNotFoundException(
-                            String.format(
-                                    "No user setting '%1$s' found for '%2$s'",
-                                    key, usr));
-                }
-                s = val + s.substring(s.indexOf(')') + 1);
-            }
-            return s;
-        } catch (ActivitiException e) {
-            String msg = String.format(
-                    "Problem whilst looking up '%1$s' for '%2$s', check with your administrator.",
-                    expr, usr);
-            LOGGER.error(msg + " " + e.getClass().getName() + ":"
-                    + e.getMessage());
-            throw new UserInfoNotFoundException(msg);
+    protected UserInfoHelper getUserInfoHelper() {
+        if (userInfoHelper == null) {
+            userInfoHelper = new UserInfoHelper();
         }
+        return userInfoHelper;
+    }
+
+    protected String lookup(DelegateExecution execution, @Nonnull String usr, @Nonnull Expression expr) {
+        return getUserInfoHelper().lookup(execution, usr, expr);
     }
 
     protected String getPassword(DelegateExecution execution, String usr) {
@@ -73,20 +52,7 @@ public abstract class BaseUserAwareTask implements JavaDelegate {
     }
 
     private String lookupBotName(DelegateExecution execution) {
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(String.format("  Looking for bot of tenant %1$s",
-                    execution.getTenantId()));
-        }
-        User botUser = execution.getEngineServices().getIdentityService()
-                .createUserQuery().userFirstName(execution.getTenantId())
-                .userLastName("Bot").singleResult();
-        if (botUser == null) {
-            String msg = String.format("No bot user for tenant '%1$s'",
-                    execution.getTenantId());
-            LOGGER.error(msg);
-            throw new ActivitiObjectNotFoundException(msg, User.class);
-        }
-        return botUser.getId();
+        return getUserInfoHelper().lookupBotName(execution);
     }
 
 }
--- work-mgmt/src/main/java/com/knowprocess/bpm/listeners/FieldInjectionListener.java
+++ work-mgmt/src/main/java/com/knowprocess/bpm/listeners/FieldInjectionListener.java
@@ -4,18 +4,31 @@ import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.ExecutionListener;
 import org.activiti.engine.delegate.Expression;
 
+import com.knowprocess.core.internal.UserInfoHelper;
+
 public class FieldInjectionListener implements ExecutionListener {
 
     private static final long serialVersionUID = 5254413459550266486L;
+
+    private UserInfoHelper userInfoHelper;
 
     private Expression varName;
 
     private Expression dynamicValue;
 
+    protected UserInfoHelper getUserInfoHelper() {
+        if (userInfoHelper == null) {
+            userInfoHelper = new UserInfoHelper();
+        }
+        return userInfoHelper;
+    }
+
     public void notify(DelegateExecution execution) throws Exception {
         if (execution.getVariable(varName.getValue(execution).toString()) == null) {
-            execution.setVariable(varName.getValue(execution).toString(),
-                    dynamicValue.getValue(execution).toString());
+            String usr = getUserInfoHelper().lookupBotName(execution);
+            String val = getUserInfoHelper().lookup(execution, usr,
+                    dynamicValue);
+            execution.setVariable(varName.getValue(execution).toString(), val);
         }
     }
 }
