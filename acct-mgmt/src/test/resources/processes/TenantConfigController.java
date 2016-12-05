e347cc837478d89cdfb0075a6d52255f81251be4
Tim Stephenson <tim@knowprocess.com>
2016-12-05 17:54:01 +0000

    logging

--- acct-mgmt/src/test/resources/processes/TenantConfigController.java
+++ acct-mgmt/src/test/resources/processes/TenantConfigController.java
@@ -2,7 +2,14 @@ package link.omny.acctmgmt.web;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import link.omny.acctmgmt.model.BotConfig;
 import link.omny.acctmgmt.model.SystemConfig;
@@ -11,6 +18,7 @@ import link.omny.acctmgmt.model.TenantAction;
 import link.omny.acctmgmt.model.TenantConfig;
 import link.omny.acctmgmt.model.TenantExtension;
 import link.omny.acctmgmt.model.TenantPartial;
+import link.omny.acctmgmt.model.TenantProcess;
 import link.omny.acctmgmt.model.TenantTemplate;
 import link.omny.acctmgmt.model.TenantToolbarEntry;
 import link.omny.acctmgmt.model.TenantTypeaheadControl;
@@ -18,21 +26,37 @@ import link.omny.acctmgmt.repositories.TenantRepository;
 import link.omny.custmgmt.model.Memo;
 import link.omny.custmgmt.repositories.ContactRepository;
 import link.omny.custmgmt.repositories.MemoRepository;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import org.activiti.engine.IdentityService;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.identity.User;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.knowprocess.bpm.api.ReportableException;
 import com.knowprocess.bpm.web.Md5HashUtils;
 import com.knowprocess.resource.spi.RestGet;
 
@@ -63,6 +87,96 @@ public class TenantConfigController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    /**
+     * Create a new tenant bot user.
+     */
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(value = "/tenants/{id}/bot", method = RequestMethod.POST)
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public @ResponseBody ResponseEntity<?> createBot(
+           @PathVariable("id") String tenantId, @RequestParam(value="force", required=false) boolean force, HttpServletRequest request) {
+
+        IdentityService idSvc = processEngine.getIdentityService();
+
+        List<User> users = idSvc.createUserQuery().userFirstName(tenantId)
+                .userLastName("Bot").list();
+        if (users.size() >= 1 && !force) {
+            throw new ReportableException(
+                    String.format(
+                            "A bot user already exists for tenant %1$s. If you're sure you want to recreate it supply the parameter 'force'",
+                            tenantId));
+        } else if (force) {
+            for (User user : users) {
+                idSvc.deleteUser(user.getId());
+            }
+        }
+
+        User botUser = idSvc.newUser(UUID.randomUUID().toString());
+        botUser.setFirstName(tenantId);
+        botUser.setLastName("Bot");
+        botUser.setPassword(UUID.randomUUID().toString());
+        idSvc.saveUser(botUser);
+
+        String url = request.getRequestURL().toString();
+        idSvc.setUserInfo(botUser.getId(), "cust-mgmt-url",
+                url.substring(0, url.indexOf("/tenants")));
+        idSvc.setUserInfo(botUser.getId(), "cust-mgmt-secret",
+                botUser.getPassword());
+        idSvc.setUserInfo(botUser.getId(), "tenant",tenantId);
+
+        idSvc.createMembership(botUser.getId(), "bot");
+
+        UriComponentsBuilder builder = MvcUriComponentsBuilder
+                .fromController(getClass());
+        HashMap<String, String> vars = new HashMap<String, String>();
+        vars.put("tenantId", tenantId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/{tenantId}/bot")
+                .buildAndExpand(vars).toUri());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    /**
+     * @param id
+     *            The id of an existing tenant.
+     * @return The complete configuration for that tenant.
+     */
+    @RequestMapping(value = "/admin/tenants", method = RequestMethod.GET, headers = "Accept=application/json")
+    public @ResponseBody List<TenantConfigSummary> showAllTenants() {
+        LOGGER.info(String.format("showAllTenants"));
+
+        // TODO not currently persisting tenants
+        // Iterable<TenantConfig> list = tenantRepo.findAll();
+        // for (TenantConfig tenant : list) {
+        List<TenantConfig> list = new ArrayList<TenantConfig>();
+        for (String tenantId : systemConfig.getTenants().split(",")) {
+            TenantConfig tenant = new TenantConfig(tenantId);
+
+            tenant.setContacts(contactRepo.countForTenant(tenantId));
+            tenant.setContactAlerts(contactRepo.countAlertsForTenant(tenantId));
+
+            tenant.setDefinitions(processEngine.getRepositoryService()
+                    .createProcessDefinitionQuery()
+                    .processDefinitionTenantId(tenant.getId()).count());
+            tenant.setActiveInstances(processEngine.getRuntimeService()
+                    .createProcessInstanceQuery()
+                    .processInstanceTenantId(tenant.getId()).count());
+            tenant.setHistoricInstances(processEngine.getHistoryService()
+                    .createHistoricProcessInstanceQuery()
+                    .processInstanceTenantId(tenant.getId()).count());
+            tenant.setJobs(processEngine.getManagementService()
+                    .createJobQuery().jobId(tenant.getId()).count());
+            tenant.setTasks(processEngine.getTaskService().createTaskQuery()
+                    .taskTenantId(tenant.getId()).count());
+            tenant.setUsers(processEngine.getIdentityService()
+                    .createUserQuery().memberOfGroup(tenant.getId()).count());
+
+            list.add(tenant);
+        }
+        return wrap(list);
+    }
 
     /**
      * @param id
@@ -95,9 +209,6 @@ public class TenantConfigController {
         } else {
             RestGet get = new RestGet();
             try {
-                LOGGER.info(String.format(
-                        "About to read config for %1$s from %2$s", id,
-                        tenant.getRemoteUrl()));
                 String sConfig = get.fetchToString(tenant.getRemoteUrl());
                 tenant.setConfig(objectMapper.readValue(sConfig,
                         new TypeReference<TenantConfig>() {
@@ -232,5 +343,51 @@ public class TenantConfigController {
             }
 
         }
+    }
+
+    private List<TenantConfigSummary> wrap(Iterable<TenantConfig> list) {
+        List<TenantConfigSummary> resources = new ArrayList<TenantConfigSummary>();
+        for (TenantConfig contact : list) {
+            resources.add(wrap(contact));
+        }
+        return resources;
+    }
+
+    private TenantConfigSummary wrap(TenantConfig tenant) {
+        TenantConfigSummary resource = new TenantConfigSummary();
+        BeanUtils.copyProperties(tenant, resource);
+        resource.setShortId(tenant.getId());
+        Link detail = linkTo(TenantRepository.class, tenant.getId())
+                .withSelfRel();
+        resource.add(detail);
+        resource.setSelfRef(detail.getHref());
+        return resource;
+    }
+
+    private Link linkTo(
+            @SuppressWarnings("rawtypes") Class<? extends CrudRepository> clazz,
+            String id) {
+        return new Link(clazz.getAnnotation(RepositoryRestResource.class)
+                .path() + "/" + id);
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    public static class TenantConfigSummary extends ResourceSupport {
+        private String shortId;
+        private String selfRef;
+        private String name;
+        private Long contacts;
+        private Long contactActions;
+        private Long contactAlerts;
+        private Long definitions;
+        private Long activeInstances;
+        private Long historicInstances;
+        private Long jobs;
+        private Long tasks;
+        private Long users;
+        private Date lastLogin;
+        private Date lastActivity;
+        private List<TenantProcess> processes;
     }
 }
