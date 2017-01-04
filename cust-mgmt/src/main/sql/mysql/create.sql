8b2284d1bbb1591270af9f437cdaece691b9fc40
Tim Stephenson <tim@knowprocess.com>
2017-01-04 18:18:51 +0000

    #387 clean up and test of acct mgmt API refer curl.txt which are all now accurate and working

--- cust-mgmt/src/main/sql/mysql/create.sql
+++
@@ -1,7 +0,0 @@
-DROP DATABASE cust_db;
-CREATE DATABASE cust_db;
--- CREATE USER 'kp'@'localhost' IDENTIFIED BY 'kp';  
-GRANT ALL ON cust_db.* TO 'kp'@'localhost'; 
-
-//DROP INDEX AK_FULL_NAME ON contact ; 
-//CREATE UNIQUE INDEX AK_FULL_NAME ON model (name, namespace);
