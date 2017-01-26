45cfc1380d4abf7f531f35009f70f78a4d87dd97
Tim Stephenson <tim@knowprocess.com>
2017-01-26 13:15:18 +0000

    clean up sloppy pom.xml

--- analytics/src/main/java/link/omny/analytics/api/AnalyticsNotFoundException.java
+++ analytics/src/main/java/link/omny/analytics/api/AnalyticsNotFoundException.java
@@ -2,6 +2,8 @@ package link.omny.analytics.api;
 
 public class AnalyticsNotFoundException extends AnalyticsException {
 
+    private static final long serialVersionUID = 9066783917708141483L;
+
     public AnalyticsNotFoundException() {
         super();
     }
