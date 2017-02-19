1a27f31ad9e1abd885f3f73b85d6ec2260c747c6
Tim Stephenson <tim@knowprocess.com>
2017-02-19 20:45:47 +0000

    #455 provide reports access to custom contact fields

--- analytics/src/main/java/link/omny/analytics/datasources/ContactDataSource.java
+++ analytics/src/main/java/link/omny/analytics/datasources/ContactDataSource.java
@@ -2,17 +2,18 @@ package link.omny.analytics.datasources;
 
 import java.util.List;
 
-import link.omny.analytics.api.ReportDataSource;
-import link.omny.custmgmt.model.Contact;
-import link.omny.custmgmt.repositories.ContactRepository;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRField;
-
 import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import link.omny.analytics.api.ReportDataSource;
+import link.omny.custmgmt.model.Contact;
+import link.omny.custmgmt.model.CustomContactField;
+import link.omny.custmgmt.repositories.ContactRepository;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
 
 @Component
 public class ContactDataSource implements ReportDataSource {
@@ -36,13 +37,31 @@ public class ContactDataSource implements ReportDataSource {
     public Object getFieldValue(JRField field) throws JRException {
         try {
             return BeanUtils
-                    .getProperty(contacts.get(row - 1), field.getName());
+                    .getNestedProperty(contacts.get(row - 1), field.getName());
+        } catch (NoSuchMethodException e) {
+            LOGGER.info(String
+                    .format("Unable to satisfy request for %1$s of contact with id %2$d, searching for custom field",
+                            field.getName(), contacts.get(row-1).getId()));
+            try {
+                for (CustomContactField customField : contacts.get(row-1).getCustomFields()) {
+                    if (customField.getName().equals(field.getName())) {
+                        return customField.getValue();
+                    }
+                }
+                return handleException(field);
+            } catch (Exception e1) {
+                return handleException(field);
+            }
         } catch (Exception e) {
-            LOGGER.warn(String
-                    .format("Unable to satisfy request for %1$s of contact with id %2$d",
-                            field.getName(), contacts.get(row).getId()));
-            return null;
+            return handleException(field);
         }
+    }
+
+    private Object handleException(JRField field) {
+        LOGGER.warn(String
+                .format("Unable to satisfy request for %1$s of contact with id %2$d",
+                        field.getName(), contacts.get(row-1).getId()));
+        return null;
     }
 
     @Override
