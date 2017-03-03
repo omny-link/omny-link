79f8158a7b08769c54058bc1601c710ebb82b0e5
Tim Stephenson <tim@knowprocess.com>
2017-03-03 18:49:48 +0000

    #466 remove entity graphs from stock item

--- catalog/src/main/java/link/omny/catalog/repositories/StockItemRepository.java
+++ catalog/src/main/java/link/omny/catalog/repositories/StockItemRepository.java
@@ -3,7 +3,6 @@ package link.omny.catalog.repositories;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -16,33 +15,26 @@ import link.omny.catalog.model.StockItem;
 public interface StockItemRepository extends CrudRepository<StockItem, Long> {
 
     @Override
-    @EntityGraph("stockItemWithCustomFields")
     StockItem findOne(@Param("id") Long id);
 
-    @EntityGraph("stockItemWithCustomFields")
     StockItem findByName(@Param("name") String name);
 
     @Query("SELECT i FROM StockItem i WHERE i.tenantId = :tenantId AND (i.status IS NULL OR i.status != 'deleted') ORDER BY i.name ASC")
-    @EntityGraph("stockItemWithAll")
     List<StockItem> findAllForTenant(@Param("tenantId") String tenantId);
 
     @Query("SELECT i FROM StockItem i WHERE i.tenantId = :tenantId AND (i.status IS NULL OR i.status != 'deleted') ORDER BY i.name ASC")
-    @EntityGraph("stockItemWithCustomFields")
     List<StockItem> findPageForTenant(@Param("tenantId") String tenantId,
             Pageable pageable);
 
     @Query("SELECT i FROM StockItem i WHERE i.tenantId = :tenantId AND LOWER(i.status) = :status ORDER BY i.name ASC")
-    @EntityGraph("stockItemWithCustomFields")
     List<StockItem> findByStatusForTenant(@Param("status") String status,
             @Param("tenantId") String tenantId);
 
     @Query("SELECT i FROM StockItem i WHERE i.tenantId = :tenantId AND LOWER(i.status) = :status ORDER BY i.name ASC")
-    @EntityGraph("stockItemWithCustomFields")
     List<StockItem> findPageByStatusForTenant(@Param("status") String status,
             @Param("tenantId") String tenantId, Pageable pageable);
 
     @Query("SELECT i FROM StockItem i INNER JOIN i.stockCategory c WHERE i.tenantId = :tenantId AND LOWER(c.name) LIKE :categoryName AND (i.status IS NULL OR i.status != 'deleted') ORDER BY i.name ASC")
-    @EntityGraph("withAll")
     List<StockItem> findAllForCategoryName(
             @Param("categoryName") String categoryName,
             @Param("tenantId") String tenantId);
