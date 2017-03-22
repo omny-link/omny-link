530debc3951c2f178f2c8b6ce8db45d3833724c2
Tim Stephenson <tim@knowprocess.com>
2017-03-22 09:12:23 +0000

    flexspace/#46 details of stock categories in item view + test

--- catalog/src/main/java/link/omny/catalog/model/StockItem.java
+++ catalog/src/main/java/link/omny/catalog/model/StockItem.java
@@ -124,7 +124,6 @@ public class StockItem implements ShortStockItem, Serializable {
     @JsonProperty
     @JsonView({ OrderViews.Summary.class, StockCategoryViews.Detailed.class,
         StockItemViews.Detailed.class })
-
     private String status;
 
     @Temporal(TemporalType.TIMESTAMP)
