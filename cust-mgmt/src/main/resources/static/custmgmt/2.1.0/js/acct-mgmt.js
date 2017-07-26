045c2581a62175396be0b9e70b60dfda343c75ba
Tim Stephenson <tim@knowprocess.com>
2017-07-26 17:12:11 +0100

    resolves #485 social fields

--- cust-mgmt/src/main/resources/static/custmgmt/2.1.0/js/acct-mgmt.js
+++ cust-mgmt/src/main/resources/static/custmgmt/2.1.0/js/acct-mgmt.js
@@ -343,7 +343,7 @@ var ractive = new BaseRactive({
           { "name": "instanceListSect", "url": "/partials/instance-list-sect.html" },
           { "name": "loginSect", "url": "/webjars/auth/1.0.0/partials/login-sect.html"},
           { "name": "profileArea", "url": "/partials/profile-area.html" },
-          { "name": "socialControls", "url": "/partials/social-controls.html" },
+          { "name": "socialModal", "url": "/partials/social-modal.html" },
           { "name": "sidebar", "url": "/partials/sidebar.html" },
           { "name": "titleArea", "url": "/partials/title-area.html" },
           /*{ "name": "mergeModal", "url": "/partials/contact-merge-sect.html" },*/
