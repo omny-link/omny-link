7633d3452f4a282adf4bbf7480054c7c2ca46da5
Tim Stephenson <tim@knowprocess.com>
2017-09-04 21:39:54 +0100

    resolves #556 confidential notes

---
+++ support-services/src/main/java/link/omny/supportservices/views/NumberSequenceViews.java
@@ -0,0 +1,12 @@
+package link.omny.supportservices.views;
+
+public class NumberSequenceViews {
+
+    public static class Summary {
+
+    }
+
+    public static class Detailed extends Summary {
+
+    }
+}
