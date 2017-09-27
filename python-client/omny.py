4c392714c7d747347658e3b57858fc92d38b161a
Tim Stephenson <tim@knowprocess.com>
2017-09-27 23:41:50 +0100

    resolves #572 archival of old processes

---
+++ python-client/omny.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import argparse
+import json
+import urllib.request
+import urllib.parse
+
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="api url to call")
+parser.add_argument("-u", "--user", required=True,
+    help="USER[:PASSWORD]  Server user and password")
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+    action="store_true")
+args = parser.parse_args()
+usr = args.user[:args.user.index(':')]
+pwd = args.user[args.user.index(':')+1:]
+ 
+# login 
+loginUrl = args.url[:args.url.index('/', 8)]+'/auth/login'
+loginPayload = '{  "username": "'+usr+'", "password": "'+pwd+'" }'
+data = loginPayload.encode('utf-8')
+headers = {
+  "X-Requested-With": "XMLHttpRequest",
+  "Content-Type": "application/json",
+  "Cache-Control": "no-cache"
+}
+
+req = urllib.request.Request(loginUrl, data, headers)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+token = json.loads(respData.decode("utf-8"))['token']
+
+# make request
+headers['X-Authorization'] = 'Bearer '+token
+
+if args.verbose:
+  print('connecting to {}'.format(args.url))
+
+req = urllib.request.Request(args.url, headers = headers)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+print(respData.decode('UTF-8'))
