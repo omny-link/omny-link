4448dc47b05a31a21446a2b2d44248c54c04f4cb
Tim Stephenson <tim@knowprocess.com>
2017-10-17 19:41:48 +0100

    Job retries fix (not enough to use setRetries API)

--- work-mgmt/src/main/java/com/knowprocess/bpm/web/JobController.java
+++ work-mgmt/src/main/java/com/knowprocess/bpm/web/JobController.java
@@ -1,9 +1,17 @@
 package com.knowprocess.bpm.web;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
+import org.activiti.engine.ManagementService;
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.impl.cmd.AbstractCustomSqlExecution;
+import org.activiti.engine.impl.cmd.CustomSqlExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.knowprocess.bpm.impl.JobRetryMapper;
 import com.knowprocess.bpm.model.Job;
 
 @RequestMapping("/{tenantId}/jobs")
@@ -25,6 +34,8 @@ public class JobController {
     @Autowired
     protected ProcessEngine processEngine;
 
+    private DateFormat iso8601Format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
     @RequestMapping(value = "", method = RequestMethod.GET, headers = "Accept=application/json")
     public @ResponseBody List<Job> showAllJson(
             @PathVariable("tenantId") String tenantId) {
@@ -32,8 +43,8 @@ public class JobController {
 
         List<org.activiti.engine.runtime.Job> list = processEngine
                 .getManagementService().createJobQuery().jobTenantId(tenantId)
+                .orderByJobDuedate().asc()
                 .list();
-
         LOGGER.info("Total jobs: " + list.size());
         return wrap(list);
     }
@@ -44,7 +55,19 @@ public class JobController {
             @PathVariable("id") String jobId) {
         LOGGER.info(String.format("retry %1$s for %2$s", jobId, tenantId));
 
-        processEngine.getManagementService().setJobRetries(jobId, 1);
+        ManagementService managementService = processEngine.getManagementService();
+        String dueDate = iso8601Format.format(new Date());
+        CustomSqlExecution<JobRetryMapper, List<Map<String, Object>>> customSqlExecution
+                = new AbstractCustomSqlExecution<JobRetryMapper, List<Map<String, Object>>>(
+                JobRetryMapper.class) {
+            public List<Map<String, Object>> execute(
+                    JobRetryMapper customMapper) {
+                customMapper.retryJob(jobId, dueDate);
+                return Collections.emptyList();
+            }
+        };
+
+        managementService.executeCustomSql(customSqlExecution);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
