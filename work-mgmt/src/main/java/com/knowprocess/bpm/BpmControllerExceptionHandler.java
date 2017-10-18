fb44625ef5fe4235c115bffbb6d78e18644d8294
Tim Stephenson <tim@knowprocess.com>
2017-10-18 12:51:28 +0100

    better exception reporting around deployment

--- work-mgmt/src/main/java/com/knowprocess/bpm/BpmControllerExceptionHandler.java
+++ work-mgmt/src/main/java/com/knowprocess/bpm/BpmControllerExceptionHandler.java
@@ -1,17 +1,22 @@
 package com.knowprocess.bpm;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.net.UnknownHostException;
 
 import org.activiti.engine.ActivitiException;
 import org.activiti.engine.ActivitiObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.knowprocess.bpm.api.BadJsonMessageException;
 import com.knowprocess.bpm.api.ReportableException;
 import com.knowprocess.bpmn.BusinessEntityNotFoundException;
@@ -20,50 +25,68 @@ import com.knowprocess.bpmn.BusinessEntityNotFoundException;
 public class BpmControllerExceptionHandler {
     protected static final Logger LOGGER = LoggerFactory
             .getLogger(BpmControllerExceptionHandler.class);
+    @Autowired
+    @Qualifier("objectMapper")
+    private ObjectMapper mapper;
+
+    protected String toJson(Exception e) {
+        StringWriter sw = new StringWriter();
+        try {
+            mapper.writeValue(sw, e);
+            return sw.toString();
+        } catch (IOException e2) {
+            LOGGER.error(e2.getMessage(), e2);
+            return String.format("{\"message\":\"%1$s\"}", e.getMessage());
+        }
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ActivitiObjectNotFoundException.class)
-    public void handleNotFound(Exception e) {
-        LOGGER.error(e.getMessage(), e);
+    public @ResponseBody String handleNotFound(Exception e) {
+        LOGGER.error(e.getMessage());
+        return toJson(e);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(BusinessEntityNotFoundException.class)
     public @ResponseBody String handleNotFoundException(
             BusinessEntityNotFoundException e) {
-        LOGGER.error(String.format("%1$s with id %2$s not found",
-                e.getEntity(), e.getId()));
+        LOGGER.error("{} with id {} not found", e.getEntity(), e.getId());
         return e.getMessage();
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ActivitiException.class)
-    public void handleInternalServerError(Exception e) {
-        LOGGER.error(e.getMessage(), e);
+    public void handleInternalServerError(ActivitiException e) {
+        LOGGER.error(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ReportableException.class)
-    public void handleReportableException(ReportableException e) {
-        LOGGER.error(e.getMessage(), e);
+    public @ResponseBody String handleReportableException(ReportableException e) {
+        LOGGER.error(e.getMessage());
+        return toJson(e);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadJsonMessageException.class)
-    public void handleBadJsonMessage(BadJsonMessageException e) {
+    public @ResponseBody String handleBadJsonMessage(BadJsonMessageException e) {
         LOGGER.error(e.getMessage(), e);
+        return toJson(e);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public void handleIllegalArgument(IllegalArgumentException e) {
+    public @ResponseBody String handleIllegalArgument(IllegalArgumentException e) {
         LOGGER.error(e.getMessage(), e);
+        return toJson(e);
     }
 
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     @ExceptionHandler(UnknownHostException.class)
-    public void handleUnknownHost(UnknownHostException e) {
+    public @ResponseBody String handleUnknownHost(UnknownHostException e) {
         LOGGER.error(e.getMessage(), e);
+        return toJson(e);
     }
 
 //    @ResponseStatus(HttpStatus.BAD_REQUEST)
