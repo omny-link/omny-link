e2d647abaad4016b394520808515309c739ecf37
Tim Stephenson <tim@knowprocess.com>
2017-12-28 16:08:18 +0000

    resolves omny/nautilus#20 save new stock

--- catalog/src/main/resources/static/catalog/2.2.0/js/stock-category.js
+++ catalog/src/main/resources/static/catalog/2.2.0/js/stock-category.js
@@ -342,7 +342,7 @@ var ractive = new BaseRactive({
       tmp.tenantId = ractive.get('tenant.id');
 //      console.log('ready to save stockCategory'+JSON.stringify(tmp)+' ...');
       $.ajax({
-        url: id === undefined ? '/'+tmp.tenantId+'/stock-categories/' : ractive.tenantUri(tmp),
+        url: id === undefined ? ractive.getServer()+'/'+tmp.tenantId+'/stock-categories/' : ractive.tenantUri(tmp),
         type: id === undefined ? 'POST' : 'PUT',
         contentType: 'application/json',
         data: JSON.stringify(tmp),
