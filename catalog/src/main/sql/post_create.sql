69cc0234b3b890af61a8508a6ab32eef42186891
Tim Stephenson <tim@knowprocess.com>
2018-01-02 08:43:31 +0000

    make stock item and cat names unique per tenant not universally

--- catalog/src/main/sql/post_create.sql
+++ catalog/src/main/sql/post_create.sql
@@ -1,5 +1,12 @@
 
 
-ALTER TABLE ol_media_res ADD CONSTRAINT FK_STOCK_ITEM_ID FOREIGN KEY (`stock_item_id`) REFERENCES `ol_stock_item` (`id`); 
+ALTER TABLE ol_media_res ADD CONSTRAINT FK_STOCK_ITEM_ID FOREIGN KEY (`stock_item_id`) REFERENCES `ol_stock_item` (`id`);
 
-ALTER TABLE ol_media_res ADD CONSTRAINT FK_STOCK_CAT_ID FOREIGN KEY (`stock_cat_id`) REFERENCES `ol_stock_cat` (`id`); 
+ALTER TABLE ol_media_res ADD CONSTRAINT FK_STOCK_CAT_ID FOREIGN KEY (`stock_cat_id`) REFERENCES `ol_stock_cat` (`id`);
+
+ALTER TABLE OL_STOCK_CAT
+ADD CONSTRAINT UK_NAME_TENANT_ID unique (name,tenant_id);
+
+
+ALTER TABLE OL_STOCK_ITEM
+ADD CONSTRAINT UK_NAME_TENANT_ID unique (name,tenant_id);
