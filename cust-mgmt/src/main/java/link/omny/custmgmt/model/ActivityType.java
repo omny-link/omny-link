935f4c075e21030eeeea8ce4a869eae94f6c40f0
Tim Stephenson <tim@knowprocess.com>
2018-09-27 23:10:40 +0100

    Add account APIs to set stage, alerts and tags

--- cust-mgmt/src/main/java/link/omny/custmgmt/model/ActivityType.java
+++ cust-mgmt/src/main/java/link/omny/custmgmt/model/ActivityType.java
@@ -1,5 +1,5 @@
 package link.omny.custmgmt.model;
 
 public enum ActivityType {
-    EMAIL, LINK_ACCOUNT_TO_CONTACT, LOGIN, REGISTRATION,
+    EMAIL, LINK_ACCOUNT_TO_CONTACT, LOGIN, REGISTRATION, TRANSITION_TO_STAGE
 }
