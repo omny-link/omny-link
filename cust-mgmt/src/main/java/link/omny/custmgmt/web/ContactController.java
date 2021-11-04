ad7f5fc262c2fba07ba048be56a4f7b5033412be
Tim Stephenson <tim@knowprocess.com>
2021-11-04 12:14:36 +0000

    add endpoint for evaluating memo template

--- cust-mgmt/src/main/java/link/omny/custmgmt/web/ContactController.java
+++ cust-mgmt/src/main/java/link/omny/custmgmt/web/ContactController.java
@@ -37,7 +37,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
