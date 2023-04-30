bc2f008ab8364dc85ac2422c658cd4d43d8ac330
Tim Stephenson <tim@knowprocess.com>
2023-04-30 16:21:13 +0100

    chore: node updates

--- crm-ui/gulpfile.js
+++ crm-ui/gulpfile.js
@@ -10,7 +10,7 @@ var replace     = require('gulp-replace');
 var rsync       = require('gulp-rsync');
 var through2    = require('through2');
 var zip         = require('gulp-zip');
-var vsn         = '3.1.5';
+var vsn         = '3.2.0';
 
 var buildDir = 'target/classes';
 var finalName = 'crm-ui-'+vsn+'.jar'
