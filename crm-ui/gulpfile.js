616532abba2db2729559759463e50af9b86a075f
Tim Stephenson <tim@knowprocess.com>
2024-08-18 23:21:30 +0100

    release: 3.1.9

--- crm-ui/gulpfile.js
+++ crm-ui/gulpfile.js
@@ -10,7 +10,7 @@ var replace     = require('gulp-replace');
 var rsync       = require('gulp-rsync');
 var through2    = require('through2');
 var zip         = require('gulp-zip');
-var vsn         = '3.1.8';
+var vsn         = '3.1.9';
 
 var buildDir = 'target/classes';
 var finalName = 'crm-ui-'+vsn+'.jar'
