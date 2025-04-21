af1ac7c2a5b7b585e265046a27868f253f2a57cd
Tim Stephenson <tim@knowprocess.com>
2025-04-21 19:12:30 +0100

    fix(ui): config keycloak

--- crm-ui2/src/lib/keycloak.js
+++ crm-ui2/src/lib/keycloak.js
@@ -1,9 +1,5 @@
 import Keycloak from 'keycloak-js';
 
-const keycloak = new Keycloak({
-  url: 'http://localhost:8080',
-  realm: 'your-realm-name',
-  clientId: 'svelte-client'
-});
+const keycloak = new Keycloak('/keycloak.json');
 
 export default keycloak;
