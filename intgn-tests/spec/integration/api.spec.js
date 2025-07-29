9bbe997362fafcf9f2d645217801b36b1e6ba6f8
Tim Stephenson <tim@knowprocess.com>
2025-07-29 13:50:51 +0100

    feat: poc of updated jasmine tests using frisby

---
+++ intgn-tests/spec/integration/api.spec.js
@@ -0,0 +1,59 @@
+const frisby = require('frisby');
+
+describe("Customer Management REST API:", function () {
+
+    const BASE_URL = "http://localhost:8082";
+    var tenantId = 'acme';
+    // var $rh = new RestEntityHelper({
+    //     server: window['$env'] == undefined ? 'http://localhost:8080' : $env.server,
+    //     tenantId: tenantId
+    // });
+
+    var originalTimeout;
+    var accountsBefore = [];
+    var contactsBefore = [];
+    var contacts = [];
+    var contact = {
+        firstName: 'Fred',
+        lastName: 'Flintstone',
+        email: 'fred@slaterock.com',
+        customFields: {
+            dateOfBirth: '01/01/1970'
+        }
+    };
+    var account = {
+        name: 'Bedrock Slate and Gravel'
+    };
+    var note = {
+        author: 'boss@slaterock.com',
+        content: 'Fred is a great worker'
+    }
+    var doc = {
+        author: 'boss@slaterock.com',
+        name: 'Annual review',
+        url: 'http://slaterock.com/reviews/Fred.odt',
+        favorite: true
+    }
+    var activity = {
+        content: 'Annual review',
+        type: 'TEST'
+    }
+    describe("GET baseline data", function () {
+        it("searches for existing contacts and accounts", function (done) {
+            frisby.get(`${BASE_URL}/${tenantId}/contacts/`)
+                .then(function (response) {
+                    console.warn(`Baseline contacts: ${response.json.length}`);
+                    expect(response.status).toBe(200);
+                    contactsBefore = response.json;
+
+                    frisby.get(`${BASE_URL}/${tenantId}/accounts/`)
+                        .then(function (response) {
+                            console.warn(`Baseline accounts: ${response.json.length}`);
+                            expect(response.status).toBe(200);
+                            accountsBefore = response.json;
+                        })
+                        .done(done);
+                });
+        });
+    });
+});
