34d7fb7f8ae0b5e6c51d8c123da200a9ac15f63f
Tim Stephenson <tim@knowprocess.com>
2025-12-29 11:51:49 +0000

    fix(crm-ui2): build

--- crm-ui2/svelte.config.js
+++ crm-ui2/svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html',
+			strict: false
+		})
+	}
 };
 
 export default config;
